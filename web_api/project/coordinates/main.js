9a337fa74746da1d669598b339a831bdb3b356d3
yujin <wwonyujin@gmail.com>
2022-10-17 18:13:16 +0900

    refactor: 💡 coordinates 프로젝트 성능 개선

--- web_api/project/coordinates/main.js
+++ web_api/project/coordinates/main.js
@@ -1,19 +1,27 @@
-const image = document.querySelector(".target-image");
-const coordinates = document.querySelector(".coordinates");
-const horizontal = document.querySelector(".horizontal");
-const vertical = document.querySelector(".vertical");
+const image = document.querySelector('.target-image');
+const coordinates = document.querySelector('.coordinates');
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
 
-window.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+// defer 옵션 사용 시 이미지가 준비되지 않아서 getBoundingClientRect() 호출해도 width, height가 0일 수 있음
+// 따라서 모든 리소스가 준비된 뒤 아래 코드르 실행하도록 이벤트리스너를 추가
+addEventListener('load', () => {
+  const imageRect = image.getBoundingClientRect();
+  const imageHalfWidth = imageRect.width / 2;
 
-  coordinates.innerHTML = `${x}px, ${y}px`;
-  coordinates.style.top = `${y}px`;
-  coordinates.style.left = `${x}px`;
+  window.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  image.style.top = `${y}px`;
-  image.style.left = `${x}px`;
+    coordinates.innerHTML = `${x}px, ${y}px`;
+    coordinates.style.transform = `translate(${x + 10}px, ${y + 10}px)`;
 
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
+    // image.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
+    image.style.transform = `translate(${x - imageHalfWidth}px, ${
+      y - imageHalfWidth
+    }px)`;
+
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+  });
 });
