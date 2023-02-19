239fa87525f63d1fa03ce5cd214eaed3511f6ddc
yujin <wwonyujin@gmail.com>
2023-02-19 21:16:33 +0900

    feat: 🎸 dom shopping list 프로젝트 실습

--- dom/project/shopping_list/main.js
+++ dom/project/shopping_list/main.js
@@ -2,22 +2,41 @@ const addForm = document.querySelector('.add-form');
 const input = document.querySelector('#item-input');
 const shoppingList = document.querySelector('.shopping-list');
 
-addForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const shoppingItem = document.createElement('li');
-  const deleteButton = document.createElement('button');
-  shoppingItem.classList.add('shopping-item');
-  shoppingItem.textContent = input.value;
-  deleteButton.textContent = 'X';
-  shoppingItem.appendChild(deleteButton);
+function onAdd() {
+  const inputValue = input.value;
+  if (inputValue.length === 0) {
+    input.focus();
+    return;
+  }
+  const item = createItem(inputValue);
+  shoppingList.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
   input.value = '';
+  input.focus();
+}
 
-  shoppingList.appendChild(shoppingItem);
-});
+function createItem(text) {
+  const shoppingItem = document.createElement('li');
+  shoppingItem.classList.add('shopping-item');
 
-shoppingList.addEventListener('click', (event) => {
-  if (event.target.tagName === 'UL') return;
-  const targetItem = event.target.closest('li');
-  shoppingList.removeChild(targetItem);
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button');
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+  deleteButton.addEventListener('click', () => {
+    shoppingList.removeChild(shoppingItem);
+  });
+
+  const itemText = document.createElement('span');
+  itemText.textContent = text;
+
+  shoppingItem.appendChild(itemText);
+  shoppingItem.appendChild(deleteButton);
+  return shoppingItem;
+}
+
+addForm.addEventListener('submit', (event) => {
+  console.log('submit');
+  event.preventDefault();
+  onAdd();
 });
